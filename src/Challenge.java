a13fd0a2ef7c24b51047a7750429071893268857
mdabuzafar <mdabuzafar78@gmail.com>
2025-05-05 17:33:10 +0530

    Improve the method

--- src/Challenge.java
+++ src/Challenge.java
@@ -1,14 +1,14 @@
 public class Challenge {
     public static void main(String[] args) {
-        calculateScore();
-    }
-
-    public static void calculateScore() {
         boolean gameOver = true;
         int score = 10_000;
         int levelCompleted = 8;
         int bonus = 200;
 
+        calculateScore(gameOver, score, levelCompleted, bonus);
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         int finalScore = score;
 
         if(gameOver) {
